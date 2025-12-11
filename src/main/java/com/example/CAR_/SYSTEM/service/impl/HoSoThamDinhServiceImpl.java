44e5071b45d716925b269daa32bad68bf9c451f2
dTienDung <riversolari@gmail.com>
2025-12-11 11:09:37 +0700

    xóa lịch sử xe

--- src/main/java/com/example/CAR_/SYSTEM/service/impl/HoSoThamDinhServiceImpl.java
+++ src/main/java/com/example/CAR_/SYSTEM/service/impl/HoSoThamDinhServiceImpl.java
@@ -28,7 +28,6 @@ public class HoSoThamDinhServiceImpl implements HoSoThamDinhService {
     private final GoiBaoHiemRepository goiBaoHiemRepository;
     private final TieuChiThamDinhRepository tieuChiThamDinhRepository;
     private final ChiTietThamDinhRepository chiTietThamDinhRepository;
-    private final LichSuTaiNanRepository lichSuTaiNanRepository;
     private final MaTranTinhPhiRepository maTranTinhPhiRepository;
 
 
